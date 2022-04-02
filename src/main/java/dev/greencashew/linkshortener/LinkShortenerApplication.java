e35361790099bae5a59207b3eadbbb3d40ced8e9
iCeBULLL <iCeBULLL@users.noreply.github.com>
2022-04-02 16:16:11 +0200

    add spring project

--- src/main/java/dev/greencashew/linkshortener/LinkShortenerApplication.java
+++ src/main/java/dev/greencashew/linkshortener/LinkShortenerApplication.java
@@ -3,11 +3,16 @@ package dev.greencashew.linkshortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
+
 @SpringBootApplication
 public class LinkShortenerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinkShortenerApplication.class, args);
+		System.out.println("Hello");
 	}
+
 
 }
